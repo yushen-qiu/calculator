8a19c19707eeb134f8af86f6e8cd6f68584319c5
Daniel Qiu <61307453+yushen-qiu@users.noreply.github.com>
2025-04-21 15:13:39 +1000

    Complete project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # calculator
+
+This is a basic calculator web app built as part of The Odin Project. It supports basic arithmetic operations (+, -, *, /), decimal input, and features like clear, delete, and sign toggle.
--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 function add(a, b) {
-
-    console.log(a+b)
     return a + b
 }
 
@@ -16,32 +14,150 @@ function divide(a, b) {
     return a / b
 }
 
-let a, b, operator
-
 function operate(a, b, operator) {
+    console.log(a)
+    console.log(b)
+    console.log(operator)
     if (operator == '+') {
-        add(a, b)
+        return add(a, b)
     } else if (operator == '-') {
-        subtract(a, b)
-    } else if (operator == '+') {
-        multiply(a, b)
-    } else if (operator == '+') {
-        divide(a, b)
+        return subtract(a, b)
+    } else if (operator == '*') {
+        return multiply(a,b)
+    } else if (operator == '/') {
+        return divide(a, b)
     }
 }
+
+let a = "0", b = "", operator = ""
+let shouldClearDisplay = true;
 
 const display = document.querySelector(".display");
 
 const numbers = document.querySelectorAll('.number');
 
+const operators = document.querySelectorAll('.operator')
 
-function addNumDisplay(a) {
-    display.append(a)
+const equals = document.querySelector(".equals")
+
+const clear = document.querySelector(".clear")
+
+const decimal = document.querySelector(".decimal")
+
+const del = document.querySelector(".delete")
+
+const sign = document.querySelector(".sign")
+
+function updateDisplay() {
+    display.textContent = a + operator + b
 }
 
-numbers.forEach(number => 
+updateDisplay()
+
+numbers.forEach(number =>   
     number.addEventListener("click", () => {
-        display.append(number.textContent)
+        if (operator == "") {
+            if (shouldClearDisplay) {
+                a = number.textContent
+                shouldClearDisplay = false;
+            } else {
+                if (a == 0 && number.textContent != 0 && !a.includes('.')) {
+                    a = number.textContent
+                } else {
+                    a += number.textContent
+                }
+            }
+        } else {
+            b += number.textContent
+        }
+        updateDisplay()
     })
-    
 )
+
+operators.forEach(o => 
+    o.addEventListener("click", () => {
+        if (a) {
+            if (operator && b) {
+                result = operate(Number(a), Number(b), operator).toFixed(6).replace(/\.?0+$/, '');
+                a = result
+                operator = ""
+                b = ""
+                if (result == Infinity || result == undefined || result == NaN) {
+                    a = "ERROR"
+                }
+            } 
+            operator = o.textContent
+            
+        }    
+        updateDisplay()
+    })
+)
+
+equals.addEventListener("click", () => {
+    result = operate(Number(a), Number(b), operator).toFixed(6).replace(/\.?0+$/, '');
+    a = result
+    operator = ""
+    b = ""
+    shouldClearDisplay = true;
+    
+    if (result == Infinity || result == undefined || result == NaN) {
+        a = "ERROR"
+    }
+    updateDisplay()
+})
+
+
+clear.addEventListener("click", () => {
+    a = "0"
+    operator = ""
+    b = ""
+    updateDisplay() 
+})
+
+decimal.addEventListener("click", () => {
+    if (operator == "" && !a.includes(".")) {
+        a += decimal.textContent
+        shouldClearDisplay = false;
+    } else if (operator != "" && !b.includes(".")) {
+        if (!b) {
+            b += "0"
+        }
+            b += decimal.textContent
+    }
+    updateDisplay() 
+}) 
+
+
+del.addEventListener("click", () => {
+    if (operator == "" && a) {
+        if (a.length != 1) {
+            a = a.substring(0, a.length - 1)
+        } else {
+            a = "0"
+        }
+    } else if (operator != "") {
+        if (!b) {
+            operator = ""
+        } else {
+            b = b.substring(0, b.length - 1)
+        }
+    } 
+    updateDisplay() 
+}) 
+
+sign.addEventListener("click", () => {
+    if (operator == "") {
+        if (a.includes("-")) {
+            a = a.substring(1, a.length)
+        } else {
+            a = "-" + a
+        }
+    } else {
+        if (b.includes("-")) {
+            b = b.substring(1, a.length)
+        } else {
+            b = "-" + b
+        }
+    }
+    updateDisplay() 
+}) 
